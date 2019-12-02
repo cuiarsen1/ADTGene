be730ebba115dee98da75b145fabfa0c73ee926e
unknown <325282390@TDSB.ON.CA>
2019-12-02 10:06:30 -0500

    Initial commit

---
+++ src/Chemical.java
@@ -0,0 +1,19 @@
+
+public class Chemical {
+
+	private char type;
+	
+	private Chemical next;
+	private Chemical prev;
+	
+	private int length;
+	
+	public Chemical(char t)
+	{
+		type = t;
+	}
+	
+	public void setNext() {
+		
+	}
+}
